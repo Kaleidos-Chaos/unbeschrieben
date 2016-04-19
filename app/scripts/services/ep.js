b4fcb8795c3370961231412e7a86ec1094276602
Xiphe <github@xiphe.net>
2016-04-20 00:13:09 +0200

    fix(ep): serve via aws

--- app/scripts/services/ep.js
+++ app/scripts/services/ep.js
@@ -19,31 +19,31 @@ angular.module('unbeschriebenEpApp')
       {
         name: 'Zeig mir die Welt',
         view: 'views/tracks/zmdw.html',
-        src: 'https://dl.dropboxusercontent.com/s/j9jmcrpkbu81r8n/kaleidos_welt_master2.mp3?dl=0',
+        src: 'https://s3.eu-central-1.amazonaws.com/unbeschrieben/zeig_mir_die_welt.mp3',
         type: 'audio/mpeg'
       },
       {
         name: 'Absinth',
         view: 'views/tracks/absinth.html',
-        src: 'https://dl.dropboxusercontent.com/s/ts51xd0yikelf21/kaleidos_absinth_master2.mp3?dl=0',
+        src: 'https://s3.eu-central-1.amazonaws.com/unbeschrieben/absinth.mp3',
         type: 'audio/mpeg'
       },
       {
         name: 'Bruder',
         view: 'views/tracks/bruder.html',
-        src: 'https://dl.dropboxusercontent.com/s/o65nuj5qe7xoi9p/kaleidos_bruder_master2.mp3?dl=0',
+        src: 'https://s3.eu-central-1.amazonaws.com/unbeschrieben/bruder.mp3',
         type: 'audio/mpeg'
       },
       {
         name: 'Alles Brennt',
         view: 'views/tracks/allesbrennt.html',
-        src: 'https://dl.dropboxusercontent.com/s/oelbai3arov7wqe/kaleidos_alles_brennt_master2.mp3?dl=0',
+        src: 'https://s3.eu-central-1.amazonaws.com/unbeschrieben/alles_brennt.mp3',
         type: 'audio/mpeg'
       },
       {
         name: 'Das Versprechen',
         view: 'views/tracks/versprechen.html',
-        src: 'https://dl.dropboxusercontent.com/s/0m285m9x9bd0ror/kaleidos_versprechen_master2.mp3?dl=0',
+        src: 'https://s3.eu-central-1.amazonaws.com/unbeschrieben/das_versprechen.mp3',
         type: 'audio/mpeg'
       }
     ];
